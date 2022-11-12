504ed6027bf5546356af352fe3f9660fafd6b8f3
jbeat630 <light0630@nate.com>
2022-11-13 00:15:41 +0900

    feat: 리액트 라우터 설치 및 리드미 작성시작, 라우터 페이지 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from "./pages/MainPage";
+import MovieDetail from "./pages/MovieDetail";
+import TvPage from "./pages/TvPage";
+import TvDetail from "./pages/TvDetail";
 
 function App() {
   return (
-    <div className="App">
-      Hello!!
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<MainPage />}/>
+              <Route path="/tv" element={<TvPage />}/>
+              <Route path="/movie/:id" element={<MovieDetail />}/>
+              <Route path="/tv/:id" element={<TvDetail />}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
