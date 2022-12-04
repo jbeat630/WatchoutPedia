0f2bcf232f60a0e6605dd1d16df2459cc728490f
kangjoobit <light0630@nate.com>
2022-12-04 17:41:44 +0900

    Feat: movie popular/topRate 추가

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -3,12 +3,16 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import LatestMovieSection from "../features/movie/latest";
 import NowPlayingSection from "../features/movie/nowPlaying";
+import PopularMovieSection from "../features/movie/popular";
+import TopRaterSection from "../features/movie/topRate";
 const MainPage: React.FC = () => {
     return (
         <div>
             <Header/>
             <LatestMovieSection/>
             <NowPlayingSection />
+            <PopularMovieSection />
+            <TopRaterSection />
             <Footer/>
         </div>
     );
