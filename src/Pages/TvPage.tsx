08b70780812d41af50b539d2b38436559dbcd04c
kangjoobit <light0630@nate.com>
2023-01-04 01:44:06 +0900

    Feat: tv latest 추가 티비 작업시작

--- src/Pages/TvPage.tsx
+++ src/Pages/TvPage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
-
+import LatestTvSection from "../features/tv/latest";
 const TvPage: React.FC = () => {
     return  (
         <div>
             <Header />
-            <div>Tv Page</div>
+            <div>
+                <LatestTvSection />
+            </div>
             <Footer />
         </div>
     )
